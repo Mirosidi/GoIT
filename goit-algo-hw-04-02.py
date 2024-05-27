876226f870a765e3775a49d46eb2d96744badfe4
Oleksii Mirosidi <mirosidialexey@gmail.com>
2024-05-27 22:43:43 +0300

    GoIT

--- goit-algo-hw-04-02.py
+++ goit-algo-hw-04-02.py
@@ -1,15 +1,19 @@
+import os
+
 def get_cats_info(path):
     cats_info = []
+    file_path = os.path.join(os.path.dirname(__file__), path)
     try:
-        with open(path, 'r', encoding='utf-8') as file:
+        with open(file_path, 'r', encoding='utf-8') as file:
             for line in file:
                 cat_id, name, age = line.strip().split(',')
                 cats_info.append({"id": cat_id, "name": name, "age": age})
     except FileNotFoundError:
-        print(f"File '{path}' not found.")
+        print(f"File '{file_path}' not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
     return cats_info
 
-cats_info = get_cats_info("D:/Python/GoIT/path.txt")
+path = "path.txt"
+cats_info = get_cats_info(path)
 print(cats_info)
