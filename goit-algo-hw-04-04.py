5ba7f29758ddcd983a7a34dfc7050a3f043c6d42
Oleksii Mirosidi <mirosidialexey@gmail.com>
2024-05-28 22:19:39 +0300

    GoIT

--- goit-algo-hw-04-04.py
+++ goit-algo-hw-04-04.py
@@ -12,8 +12,9 @@ def change_contact(name, new_phone):
         print("Contact not found.")
 
 def show_phone(name):
-    if name in contacts:
-        print(contacts[name])
+    phone = contacts.get(name)
+    if phone is not None:
+        print(phone)
     else:
         print("Contact not found.")
 
@@ -32,24 +33,24 @@ def main():
     while True:
         user_input = input("Enter a command: ")
         command, args = parse_input(user_input)
-        if command == "exit" or command == "close" or command == "stop":
+        if command in ["exit", "close", "stop"]:
             print("Good bye!")
             break
         elif command == "hello":
             print("How can I help you?")
         elif command == "add":
             if len(args) == 2:
-                add_contact(args[0], args[1])
+                add_contact(*args)
             else:
                 print("Invalid command format. Use: add [name] [phone]")
         elif command == "change":
             if len(args) == 2:
-                change_contact(args[0], args[1])
+                change_contact(*args)
             else:
                 print("Invalid command format. Use: change [name] [new_phone]")
         elif command == "phone":
             if len(args) == 1:
-                show_phone(args[0])
+                show_phone(*args)
             else:
                 print("Invalid command format. Use: phone [name]")
         elif command == "all":
