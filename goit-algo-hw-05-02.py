b0fc1abbaa3dd4f9f49945bb53603492801892fa
Oleksii Mirosidi <mirosidialexey@gmail.com>
2024-05-31 00:06:26 +0300

    GoIT

--- goit-algo-hw-05-02.py
+++ goit-algo-hw-05-02.py
@@ -1,16 +1,17 @@
 import re
+from typing import Generator, Callable
 
 our_text = input("text: ")
 
-def generator_numbers(text: str):
+def generator_numbers(text: str) -> Generator[float, None, None]:
     pattern = re.compile(r'\b\d+(\.\d+)?\b')
     matches = pattern.finditer(text)
     for match in matches:
         number = float(match.group())
         yield number
 
-def sum_profit(text: str):
-    total_sum = sum(generator_numbers(text))
+def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
+    total_sum = sum(func(text))
     print("Total profit:", total_sum)
 
-sum_profit(our_text)
+sum_profit(our_text, generator_numbers)
